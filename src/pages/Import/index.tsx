119c68851af7934fbc00e7eb26dbe206b4f7383a
Deivison Cardoso <deivisondc@gmail.com>
2020-11-24 21:00:36 -0300

    connect import with api

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    if (uploadedFiles && uploadedFiles.length > 0) {
+      const data = new FormData();
+      data.append('file', uploadedFiles[0].file);
 
-    // TODO
+      try {
+        await api.post('/transactions/import', data);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+        history.push('/');
+      } catch (err) {
+        console.log(err.response.error);
+      }
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map(file => ({
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      })),
+    );
   }
 
   return (
